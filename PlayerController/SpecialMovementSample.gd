f5e7618042afe020cce72b7138095bac11a10ba4
NahtreX <37822661+NahtreX@users.noreply.github.com>
2024-11-20 16:27:47 -0300

    Update SpecialMovementSample.gd

--- PlayerController/SpecialMovementSample.gd
+++ PlayerController/SpecialMovementSample.gd
@@ -6,7 +6,8 @@ extends SpecialMovementsPlatformer2D
 ## requiredCommands = PackedStringArray(["dash"])
 ## requiredAnimations = PackedStringArray(["dash"])
 func _set_commands_and_animations() -> void:
-	pass
+	# This name is shown in the debug menu. Replace it to fit your needs
+	movementName = "SpecialMovement"
 
 ## Setup function.
 func _on_update() -> void:
@@ -31,3 +32,22 @@ func _jump_override() -> bool:
 ## Sprite flip check function. Return true if you need the sprite to not flip under certain circumstances.
 func _flip_check() -> bool:
 	return false
+
+## Exports variables for debug testing live.
+func _get_debug_variables() -> DebugMenuEditor.ParameterCategory:
+	var category: DebugMenuEditor.ParameterCategory = DebugMenuEditor.ParameterCategory.new()
+	category.category = movementName
+	category.contents = [
+		# Add contents following the formula:
+		# parameter is the name of the variable as is in this script
+		# type is the type of variable to use. Your pick from NUMERIC, BOOL, LIST.
+		# defaultValue should be set to whatever the default value is. Unless there's extra steps involved you can simply put the variable you are exposing here.
+		# extra data is used for NUMERIC and LIST types only:
+		# NUMERIC extra data is a DebugParameterContainer.NumericData containing min_value, max_value, and step.
+		# You can crate it using DebugParameterContainer.NumericData.new(min_value, max_value, step)
+		# LIST extra data is an array of Strings to show in the list component.
+		# Sample line:
+		# DebugMenuEditor.ParameterContents.new(parameter: String, type: DebugParameterContainer.ParameterTypes, defaultValue: Variant, extraData: Variant)
+		# Please check other included movement types for more examples of this being used.
+	]
+	return category
